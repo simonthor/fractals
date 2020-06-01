3e4c314c1400c345527c649383409b622e9430c5
Simon Thor <simon11.thor@hotmail.se>
2020-06-01 23:40:03 +0200

    Made mandelbrot function more general to an escape time fractal generator

--- fractal_generator.py
+++ fractal_generator.py
@@ -1,9 +1,11 @@
 import numpy as np
 from matplotlib import pyplot as plt
+from typing import Callable, Tuple
 
 
-def generate_mandelbrot(plane: np.ndarray, iterations: int):
-    """Generates the mandelbrot set.
+def generate_escape_time(plane: np.ndarray, iterations: int, et_function: Callable[[np.ndarray, np.ndarray], np.ndarray],
+                         *et_f_args, **et_f_kwargs) -> Tuple[np.ndarray, np.ndarray]:
+    """Generates an escape time fractal.
 
     Parameters
     ----------
@@ -11,6 +13,13 @@ def generate_mandelbrot(plane: np.ndarray, iterations: int):
         The coordinates in the complex plane that will be used to generate the mandelbrot set.
     iterations : int
         Maximum number of iterations that the mandelbrot formula will be used on a given number.
+    et_function : function
+        The function used to calculate the z value of each point in the complex plane.
+        Must at least accept two input arguments of the complex plane and the current z values.
+    *et_f_args : any, optional
+        Other input arguments for et_function.
+    **et_f_kwargs : any, optional
+        Other input keyword arguments for et_function.
 
     Returns
     ----------
@@ -30,21 +39,25 @@ def generate_mandelbrot(plane: np.ndarray, iterations: int):
     for i in range(iterations):
         exceeded_limit_after = np.abs(z_values) >= 2
         iteration_counter[exceeded_limit_after & ~exceeded_limit_before] = i
-        z_values[~exceeded_limit_after] = z_values[~exceeded_limit_after]**2 + plane[~exceeded_limit_after]
+        z_values[~exceeded_limit_after] = et_function(z_values[~exceeded_limit_after], plane[~exceeded_limit_after],
+                                                      *et_f_args, **et_f_kwargs)
         exceeded_limit_before = exceeded_limit_after
     return iteration_counter, z_values
 
 
 if __name__ == '__main__':
+    colormap = 'twilight'
     re_lim = (-2., 1.)
     im_lim = (-1.5, 1.5)
-    resolution = 1000
+    resolution = 500
     iterations = 100
-    complex_plane = np.linspace(*re_lim, resolution) + 1j*np.linspace(*im_lim, resolution).reshape((resolution, 1))
-    fractal_image, z_values = generate_mandelbrot(complex_plane, iterations)
+    et_function = lambda z, c: (np.abs(np.real(z)) + 1j*np.abs(np.imag(z)))**2 + c
+
+    complex_plane = np.linspace(*re_lim, resolution) + 1j*np.linspace(*im_lim[::-1], resolution)[:, np.newaxis]
+    fractal_image, z_values = generate_escape_time(complex_plane, iterations, et_function)
     fig, axes = plt.subplots(ncols=2, sharex='all', sharey='all')
-    axes[0].imshow(fractal_image, cmap='jet', extent=re_lim+im_lim)
-    axes[1].imshow(np.abs(z_values), cmap='jet', extent=re_lim+im_lim)
+    axes[0].imshow(fractal_image, cmap=colormap, extent=re_lim+im_lim)
+    axes[1].imshow(np.abs(z_values), cmap=colormap, extent=re_lim+im_lim)
     axes[0].set_xlabel('$Re(z)$')
     axes[0].set_ylabel('$Im(z)$')
     plt.show()
